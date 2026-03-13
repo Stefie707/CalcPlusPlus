b04dc16a491323757fead4f305690bd40b6fefd3
u.s.canteen <stefanspies05@gmail.com>
2026-03-13 22:24:03 +0200

    updated
    babaab

---
+++ Tutorial/Tutorial.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// cin is with >>
+
+int main() {
+	int a;
+	int b;
+	int sum;
+
+	cout << "Enter a number. \n";
+	cin >> a;
+
+	cout << "Enter a new number. \n";
+	cin >> b;
+		sum = a + b;
+		cout << "Thee sum of those numbers is \n" << sum << endl; 
+		cout << "wowers u did it" << endl;
+		std::cin.get();
+		
+		return 0;
+		
+		
+
+}
