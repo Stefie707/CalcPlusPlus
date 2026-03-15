5ac688c4b8ffbb329a0bd42deeeefbf4a6e74ba5
u.s.canteen <stefanspies05@gmail.com>
2026-03-15 20:03:20 +0200

    Update Tutorial.cpp

--- Tutorial/Tutorial.cpp
+++ Tutorial/Tutorial.cpp
@@ -10,13 +10,16 @@ int main() {
 
 	cout << "Enter a number. \n";
 	cin >> a;
-
+if (a!=b):
+	cout << "uh oh! calculator error. Please type something else into the calculator."
 	cout << "Enter a new number. \n";
 	cin >> b;
 		sum = a + b;
 		cout << "Thee sum of those numbers is \n" << sum << endl; 
 		cout << "wowers u did it" << endl;
 		std::cin.get();
+
+		
 		
 		return 0;
 		
